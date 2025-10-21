225afeb0428a03abd97812220f992fb35a1d3161
MliliGenes <saad.elm.77@gmail.com>
2025-10-21 11:38:46 +0100

    3 levels in evaluation mode

--- js/intro.js
+++ js/intro.js
@@ -7,9 +7,12 @@ function save_login()
     localStorage.setItem("g_my_login", login);
     if (login == '')
     {
-	var lvl = Math.round(6 + 4*Math.random());
-	localStorage.setItem("g_my_eval", JSON.stringify([lvl]));
-	window.location = "level"+lvl+".html";
+	// Generate 3 random levels from 6 to 10 for evaluation mode
+	var lvl1 = Math.round(6 + 4*Math.random());
+	var lvl2 = Math.round(6 + 4*Math.random());
+	var lvl3 = Math.round(6 + 4*Math.random());
+	localStorage.setItem("g_my_eval", JSON.stringify([lvl1, lvl2, lvl3]));
+	window.location = "level"+lvl1+".html";
     }
     else
 	window.location = 'level1.html';
